a61b25477133558737b0f31c34b529d886e88197
VictorRibeiroH <contato.victoribeiro@gmail.com>
2024-12-12 06:00:13 -0300

    img s3

--- client/src/app/projects/BoardView/index.tsx
+++ client/src/app/projects/BoardView/index.tsx
@@ -173,7 +173,7 @@ const Task = ({ task }: TaskProps) => {
         >
             {task.attachments && task.attachments.length > 0 && (
                 <Image
-                    src={`/${task.attachments[0].fileURL}`}
+                    src={`https://tasklibs-s3-images.s3.us-east-1.amazonaws.com/${task.attachments[0].fileURL}`}
                     alt={task.attachments[0].fileName}
                     width={400}
                     height={200}
@@ -224,7 +224,7 @@ const Task = ({ task }: TaskProps) => {
                         {task.author && (
                             <Image
                                 key={task.author.userId}
-                                src={`/${task.author.profilePictureUrl!}`}
+                                src={`https://tasklibs-s3-images.s3.us-east-1.amazonaws.com/${task.author.profilePictureUrl!}`}
                                 alt={task.author.username}
                                 width={30}
                                 height={30}
